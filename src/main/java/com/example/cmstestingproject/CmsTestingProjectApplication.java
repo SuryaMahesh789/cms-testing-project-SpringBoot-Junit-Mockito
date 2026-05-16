d8db62235d701c99cdff185ee33abe57812fbabe
Kolisetty Surya Mahesh <kolisettysuryamahesh@gmail.com>
2026-05-16 15:04:27 +0530

    Welcome Message Added

--- src/main/java/com/example/cmstestingproject/CmsTestingProjectApplication.java
+++ src/main/java/com/example/cmstestingproject/CmsTestingProjectApplication.java
@@ -8,6 +8,7 @@ public class CmsTestingProjectApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CmsTestingProjectApplication.class, args);
+        System.out.println("Testing Application Started");
     }
 
 }

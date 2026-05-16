406e6e1c3f86c631cb095f92150d0c2977438c67
Kolisetty Surya Mahesh <kolisettysuryamahesh@gmail.com>
2026-05-16 15:03:03 +0530

    Initial commit

---
+++ src/main/java/com/example/cmstestingproject/CmsTestingProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.cmstestingproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmsTestingProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CmsTestingProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cmstestingproject/CmsTestingProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cmstestingproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmsTestingProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
